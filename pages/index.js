a9142c7057cf9bc87d5da1b9b647fbf2875ee8d1
unknown <velkovandrej819@gmail.com>
2022-04-13 00:57:11 +0200

    try

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Products from "../components/Home/Products";
 import Contact from "../components/Home/Contact";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-export async function getServerSideProps({ locale }) {
+export async function getStaticProps({ locale }) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_API}products?landingPage=true&lang=${locale}`
   );
@@ -14,6 +14,7 @@ export async function getServerSideProps({ locale }) {
       ...(await serverSideTranslations(locale, ["home"])),
       data,
     },
+    revalidate: 1,
   };
 }
 
