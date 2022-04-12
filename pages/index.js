7ae710f99fd4b2a2fd1f82bfe08870713dd295ea
unknown <velkovandrej819@gmail.com>
2022-04-13 01:05:30 +0200

    keke

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export async function getStaticProps({ locale }) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_API}products?landingPage=true&lang=${locale}`
+    `${process.env.NEXT_PUBLIC_SERVER_API}products?lang=${locale}`
   );
   const data = await res.json();
   return {
